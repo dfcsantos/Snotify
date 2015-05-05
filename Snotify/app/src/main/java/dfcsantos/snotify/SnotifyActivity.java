d3b6330319cc451fb54a9c920e4115b93dce322b
Felipe Bueno <bueno.felipe@gmail.com>
2015-05-05 20:39:48 -0300

    plug it with Sneer's Text Message api

--- Snotify/app/src/main/java/dfcsantos/snotify/SnotifyActivity.java
+++ Snotify/app/src/main/java/dfcsantos/snotify/SnotifyActivity.java
@@ -1,9 +1,8 @@
 package dfcsantos.snotify;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class SnotifyActivity extends ActionBarActivity {
@@ -11,29 +10,9 @@ public class SnotifyActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_snotify);
+
+		startService(getIntent().<Intent>getParcelableExtra("SEND_MESSAGE").setAction("http://open.spotify.com/track/1SXBC7gcwQNLzn9THgigxk"));
+		finish();
 	}
 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_snotify, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
