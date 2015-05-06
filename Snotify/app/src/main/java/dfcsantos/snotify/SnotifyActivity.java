65fe54e3fc201f4e8f6bca7bdbf93b3cb6cec9d8
Felipe Bueno <bueno.felipe@gmail.com>
2015-05-05 21:18:08 -0300

    send current spotify track to sneer using the Text Message Api

--- Snotify/app/src/main/java/dfcsantos/snotify/SnotifyActivity.java
+++ Snotify/app/src/main/java/dfcsantos/snotify/SnotifyActivity.java
@@ -1,18 +1,32 @@
 package dfcsantos.snotify;
 
-import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 
 public class SnotifyActivity extends ActionBarActivity {
 
+	SnotifyBroadcastReceiver receiver;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		startService(getIntent().<Intent>getParcelableExtra("SEND_MESSAGE").setAction("http://open.spotify.com/track/1SXBC7gcwQNLzn9THgigxk"));
+		receiver = new SnotifyBroadcastReceiver(this);
+
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(SnotifyBroadcastReceiver.BroadcastTypes.METADATA_CHANGED);
+		registerReceiver(receiver, filter);
+
+
 		finish();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(receiver);
 	}
 
 }
